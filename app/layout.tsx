04ca5ac0a5392df1ffc505c5cc36830ef8a88a72
sragss <sammage@gmail.com>
2025-09-08 10:04:54 -0400

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,17 @@ const description = `This is a demo of an end-to-end coding platform where the u
 export const metadata: Metadata = {
   title,
   description,
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico', sizes: '48x48' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
   openGraph: {
     images: [
       {
